ceb954fb0067c7de696fd2fb9fc989ad9c572245
jaydenuk <jayden.uk@daumkakao.com>
2016-04-11 20:04:44 +0900

    -- 적립포인트 계산을 Rental 객체로 이동.

--- src/main/java/pub/jayden/refactoring/videoshop/entiry/Customer.java
+++ src/main/java/pub/jayden/refactoring/videoshop/entiry/Customer.java
@@ -37,11 +37,7 @@ public class Customer {
 
             Rental each = rentalEnumeration.nextElement();
 
-            frequentRenterPoints ++;
-
-            if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() >1)
-                frequentRenterPoints ++;
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
             result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
             totalAmount += each.getCharge();
